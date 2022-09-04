fb7b69cf7eba1a2a5311869e47ef6c84e3c26229
Luong Trac Duc Anh <94946568+luongtracducanh@users.noreply.github.com>
2022-09-05 03:03:52 +0700

    Modified main.js

--- main.js
+++ main.js
@@ -1,126 +1,113 @@
 const input = require('sync-input')
 
-// let water = Number(input("Write how many ml of water the coffee machine has:\n"));
-// let milk = Number(input("Write how many ml of milk the coffee machine has:\n"));
-// let beans = Number(input("Write how many grams of coffee beans the coffee machine has:\n"));
-// let cups_needed = Number(input("Write how many cups of coffee you will need:\n"));
-// let cups_can_make = 0;
-
-// function count_cups() {
-//     while (water >= 200 && milk >= 50 && beans >= 15) {
-//         water -= 200;
-//         milk -= 50;
-//         beans -= 15;
-//         cups_can_make += 1;
-//         if (water <= 0 || milk <= 0 || beans <= 0) {
-//             return cups_can_make;
-//         }
-//     }
-// }
-
-// count_cups();
-
-// if (cups_can_make == cups_needed) {
-//     console.log(`Yes, I can make that amount of coffee`);
-// }
-// else if (cups_can_make < cups_needed) {
-//     console.log(`No, I can make only ${cups_can_make} cups of coffee`);
-// }
-// else {
-//     console.log(`Yes, I can make that amount of coffee (and even ${cups_can_make - cups_needed} more than that)`);
-// }
-
-let water = 400;
-let milk = 540;
-let beans = 120;
-let cups = 9;
-let money = 550;
+let machine = {
+	"water": 400,
+	"milk": 540,
+	"beans": 120,
+	"cups": 9,
+	"money": 550
+}
 
 const coffee = [
-    espresso = {
-        water: 250,
-        milk: 0,
-        beans: 16,
-        cup: 1,
-        money: 4
-    },
-    latte = {
-        water: 350,
-        milk: 75,
-        beans: 20,
-        cup: 1,
-        money: 7
-    },
-    cappuccino = {
-        water: 200,
-        milk: 100,
-        beans: 12,
-        cup: 1,
-        money: 6
-    }
+	espresso = {
+		"water": 250,
+		"milk": 0,
+		"beans": 16,
+		"cup": 1,
+		"money": 4,
+	},
+	latte = {
+		"water": 350,
+		"milk": 75,
+		"beans": 20,
+		"cup": 1,
+		"money": 7,
+	},
+	cappuccino = {
+		"water": 200,
+		"milk": 100,
+		"beans": 12,
+		"cup": 1,
+		"money": 6,
+	}
 ]
 
-function display_supply() {
-    console.log(`The coffee machine has:
-${water} ml of water
-${milk} ml of milk
-${beans} g of coffee beans
-${cups} disposable cups
-$${money} of money`);
+function remaining() {
+	console.log(`\nThe coffee machine has:
+${machine.water} ml of water
+${machine.milk} ml of milk
+${machine.beans} g of coffee beans
+${machine.cups} disposable cups
+$${machine.money} of money\n`);
 }
 
 function get_action() {
-    let action = input(`\nWrite action (buy, fill, take):\n`);
-    return action;
+	let action = input(`Write action (buy, fill, take, remaining, exit):\n`);
+	return action;
 }
 
 function buy() {
-    let buy_option = Number(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n"));
-
-    buy_option--;
-    water -= coffee[buy_option].water;
-    milk -= coffee[buy_option].milk;
-    beans -= coffee[buy_option].beans;
-    cups -= coffee[buy_option].cup;
-    money += coffee[buy_option].money;
+	let coffee_to_buy = input("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:\n");
+	if (coffee_to_buy == "back") {
+		return;
+	} else {
+		coffee_to_buy--;
+		if (machine.water < coffee[coffee_to_buy].water) {
+			console.log("Sorry, not enough water!\n");
+		} else if (machine.milk < coffee[coffee_to_buy].milk) {
+			console.log("Sorry, not enough milk!\n");
+		} else if (machine.beans < coffee[coffee_to_buy].beans) {
+			console.log("Sorry, not enough coffee beans!\n");
+		} else if (machine.cups < coffee[coffee_to_buy].cups) {
+			console.log("Sorry, not enough cups!\n");
+		} else {
+			machine.water -= coffee[coffee_to_buy].water;
+			machine.milk -= coffee[coffee_to_buy].milk;
+			machine.beans -= coffee[coffee_to_buy].beans;
+			machine.money += coffee[coffee_to_buy].money;
+			machine.cups -= 1;
+			console.log("I have enough resources, making you a coffee!\n");
+		}
+	}
 }
 
 function fill() {
-    let water_added = Number(input("Write how many ml of water you want to add:\n"));
-    let milk_added = Number(input("Write how many ml of milk you want to add:\n"));
-    let beans_added = Number(input("Write how many grams of coffee beans you want to add:\n"));
-    let cups_added = Number(input("Write how many disposable coffee cups you want to add:\n"));
+	let water_added = Number(input("\nWrite how many ml of water you want to add:\n"));
+	let milk_added = Number(input("Write how many ml of milk you want to add:\n"));
+	let beans_added = Number(input("Write how many grams of coffee beans you want to add:\n"));
+	let cups_added = Number(input("Write how many disposable coffee cups you want to add:\n"));
 
-    water += water_added;
-    milk += milk_added;
-    beans += beans_added;
-    cups += cups_added;
+	console.log();
+
+	machine.water += water_added;
+	machine.milk += milk_added;
+	machine.beans += beans_added;
+	machine.cups += cups_added;
 }
 
 function take() {
-    console.log(`I gave you $${money}`);
-    money = 0;
+	machine.money = 0;
+	console.log(`\nI gave you $${machine.money}\n`);
 }
 
-display_supply();
-
-let action = get_action();
-switch (action) {
-    case "buy":
-        buy();
-        console.log();
-        display_supply();
-        break;
-    case "fill":
-        fill();
-        console.log();
-        display_supply();
-        break;
-    case "take":
-        take();
-        console.log();
-        display_supply();
-        break;
-    default:
-        break;
+while (true) {
+	let new_action = get_action();
+	switch (new_action) {
+		case "buy":
+			buy();
+			break;
+		case "fill":
+			fill();
+			break;
+		case "take":
+			take();
+			break;
+		case "remaining":
+			remaining();
+			break;
+		case "exit":
+			return;
+		default:
+			break;
+	}
 }
